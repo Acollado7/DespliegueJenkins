ec1fa305f56a966f375382f3a97cf9d6ba643bd8
Acollado7 <acolara132@g.educaand.es>
2024-03-08 19:03:00 +0100

    Primer commit de la app de react para jenkins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Aplicacion de ejemplo para la practica de jenkins</h1>
+     <p>Mi compañero alvaro modificara esta linea</p>
+    </div>
+  );
+}
+
+export default App;
